9cf2f54b0962b3dfc1e73affcfc71601862238fa
freezy <freezy@kodi.tv>
2023-11-19 01:07:27 +0100

    gates: Make them transformable and kinematic.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Gate/GateApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Gate/GateApi.cs
@@ -97,11 +97,15 @@ namespace VisualPinball.Unity
 
 		#region Collider Generation
 
-		protected override void CreateColliders(ref ColliderReference colliders,
-			ref ColliderReference kinematicColliders, float margin)
+		protected override void CreateColliders(ref ColliderReference colliders, ref ColliderReference kinematicColliders, float margin)
 		{
-			var colliderGenerator = new GateColliderGenerator(this, MainComponent, ColliderComponent);
-			colliderGenerator.GenerateColliders(MainComponent.PositionZ, ref colliders);
+			var matrix = MainComponent.transform.worldToLocalMatrix.WorldToLocalTranslateWithinPlayfield(Player.PlayfieldToWorldMatrix);
+			var colliderGenerator = new GateColliderGenerator(this, MainComponent, ColliderComponent, matrix);
+			if (ColliderComponent._isKinematic) {
+				colliderGenerator.GenerateColliders(MainComponent.PositionZ, ref kinematicColliders);
+			} else {
+				colliderGenerator.GenerateColliders(MainComponent.PositionZ, ref colliders);
+			}
 		}
 
 		#endregion
